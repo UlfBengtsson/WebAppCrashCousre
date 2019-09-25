8bcf0e970e244cb632b1b71d08b915a2e8205dbd
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-09-25 15:47:43 +0200

    Add project files.

---
+++ WebAppCrashCousre/Controllers/CarsController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAppCrashCousre.Models;
+
+namespace WebAppCrashCousre.Controllers
+{
+    public class CarsController : Controller
+    {
+        static List<Car> cars = new List<Car>() { new Car("SAAB", "900") };
+
+        // GET: Cars
+        public ActionResult Index()
+        {
+            return View("Index", cars);
+        }
+
+        // GET: Cars/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Cars/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cars/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Car car)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    cars.Add(car);
+
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    return View(car);
+                }
+
+            }
+            catch
+            {
+                return View(car);
+            }
+        }
+
+        // GET: Cars/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Cars/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cars/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Cars/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebAppCrashCousre/Models/Car.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCrashCousre.Models
+{
+    public class Car
+    {
+        [Key]
+        public int Id { get; private set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string Brand { get; set; }
+
+        [Required]
+        [Display(Name ="Model")]
+        [StringLength(100, MinimumLength = 1)]
+        public string ModelName { get; set; }
+
+        /*
+        int age;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    age = value;
+                }
+                else
+                {
+                    throw new NotSupportedException();
+                }
+            }
+        }
+        */
+
+        public Car()
+        {
+
+        }
+
+        public Car(string brand, string modelName)
+        {
+            Brand = brand;
+            ModelName = modelName;
+        }
+    }
+}
